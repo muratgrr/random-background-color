4b21d6dcd4a29b46d4736b9d5a1365dbf6ae7ebc
muratgrr <m.gurer3365@gmail.com>
2023-01-17 22:28:20 +0300

    update

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const p = document.createElement("p");
 button.appendChild(p);
 
 button.addEventListener("click", function () {
-  const rasSayi1 = Math.floor(Math.random() * 256);
-  const rasSayi2 = Math.floor(Math.random() * 256);
-  const rasSayi3 = Math.floor(Math.random() * 256);
-  const rasRenk = `RGB(${rasSayi1}, ${rasSayi2}, ${rasSayi3})`;
+  const randomNum1 = Math.floor(Math.random() * 256);
+  const randomNum2 = Math.floor(Math.random() * 256);
+  const randomNum3 = Math.floor(Math.random() * 256);
+  const rasRenk = `RGB(${randomNum1}, ${randomNum2}, ${randomNum3})`;
   body.style.backgroundColor = rasRenk;
   //   const text = document.createTextNode("RENK : ");
   //   p.appendChild(text);
-  p.innerText = "RENK : " + rasRenk;
+  p.innerText = "COLOR : " + rasRenk;
 });
